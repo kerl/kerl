d1bfaabd4c7e593b502204a7fba168d43c3f81f8
Dmitriy Kargapolov <dmitriy.kargapolov@gmail.com>
2023-09-08 07:52:55 -0400

    Fix Lint, CI, activate test

--- tests/activate_test.sh
+++ tests/activate_test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mod_env() {
+expected_env() {
     DIR="$1"
     OLD="$2"
-    cat "$OLD" |grep -wv -e PATH -e MANPATH -e PS1
+    grep "$OLD" -wv -e PATH -e MANPATH -e PS1
     cat - <<EOT
 _KERL_ACTIVE_DIR=$DIR
 _KERL_ERL_AFLAGS_SET=
@@ -19,7 +19,12 @@ PS1=$PS1
 REBAR_CACHE_DIR=$DIR/.cache/rebar3
 REBAR_PLT_DIR=$DIR
 EOT
-    ERLCALLDIR=`\find "$DIR" -type d -path "*erl_interface*/bin" 2>/dev/null`
+    if grep -qw MANPATH "$OLD"; then
+        echo "_KERL_MANPATH_SET=x"
+    else
+        echo "_KERL_MANPATH_SET="
+    fi
+    ERLCALLDIR=$(\find "$DIR" -type d -path "*erl_interface*/bin" 2>/dev/null)
     if [ -n "$ERLCALLDIR" ]; then
         echo "_KERL_ERL_CALL_REMOVABLE=$ERLCALLDIR"
         echo "PATH=$ERLCALLDIR:$DIR/bin:$PATH"
@@ -33,20 +38,21 @@ test_it() {
     ./kerl emit-activate "$1" "$1" "$2" >/tmp/activate.sh
     export PS1="test> "
     env | sort >/tmp/env_old
+    # shellcheck source=/dev/null
     . /tmp/activate.sh
     env | sort >/tmp/env_act
     kerl_deactivate
     env | sort >/tmp/env_new
 
-    mod_env $2 /tmp/env_old | sort >/tmp/env_mod
-    diff /tmp/env_mod /tmp/env_act || { echo "env setup failed"; exit 1; }
+    expected_env "$2" /tmp/env_old | sort >/tmp/env_exp
+    diff /tmp/env_exp /tmp/env_act || { echo "env setup failed"; exit 1; }
     diff /tmp/env_old /tmp/env_new || { echo "env cleanup failed"; exit 1; }
 }
 
 ver=$1
 
-existing_dir=`./kerl path "$ver"`
-[ -d "$existing_dir" ] || exit -1
+existing_dir=$(./kerl path "$ver")
+[ -d "$existing_dir" ] || exit 1
 
 test_it "$ver" "$existing_dir"
 test_it foo foo_dir
